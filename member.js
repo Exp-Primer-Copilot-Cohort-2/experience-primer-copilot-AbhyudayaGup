ac32ca754e9703d5630f0fe03fd2f7516d0ad9ff
AbhyudayaGup <153574809+AbhyudayaGup@users.noreply.github.com>
2024-08-02 18:20:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = {
+      name: 'John Doe',
+      age: 26,
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      address: {
+        city: 'Boston',
+        state: 'MA',
+        country: 'USA'
+      }
+    };
+    return member;
+  }
